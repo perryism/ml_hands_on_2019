d949790360bf9d939e0cfb61fc6b3eb2d5fe561e
Perry <perryism@gmail.com>
2019-07-13 12:08:26 -0700

    Launch notebook as soon as it's ready

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+set -e
+
+function launch_browser() {
+	until docker logs ml101 2>&1 | grep "The Jupyter Notebook is running at" ; do sleep 1; done
+
+	open http://localhost:8888
+}
+
+launch_browser &
 docker run -it --rm --name ml101 -p 8888:8888 -v $(pwd)/notebooks/:/workspace/notebooks ml101 
